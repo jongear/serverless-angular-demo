2b8a5c7c33d14628b715a0df0e8c22045a16a435
Jonathan Gear <askjon@jongear.io>
2016-08-03 15:31:30 -0500

    adding post template

--- functions/heroes/handler.js
+++ functions/heroes/handler.js
@@ -7,13 +7,17 @@ module.exports.handler = function(event, context, cb) {
 
   console.log('Received event:',JSON.stringify(event,null,2));
   console.log('Context:',JSON.stringify(context,null,2));
+  console.log('operation', event.operation);
 
   var operation = event.operation;
   if(event.tableName) {
     event.payload.TableName = event.tableName;
+    console.log('table name', event.tableName);
   }
 
   switch(operation){
+    case 'create':
+      dynamo.putItem(event.payload,context.succeed());
     case 'read':
       dynamo.getItem(event.payload,context.done);
       break;
